fbc20678b10de5d1919d70a4350e29a809ec7f55
Luis Felipe <sluisfelipe20@outlook.com>
2024-08-01 11:54:29 -0300

    Base do jogo

---
+++ All Against Me/Assets/Script/Player.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float vel;
+    Rigidbody2D rb;
+    Vector3 moveVector;
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        moveVector = new Vector3();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveVector.x = Input.GetAxisRaw("Horizontal");
+        moveVector.y = Input.GetAxisRaw("Vertical");
+        moveVector *= vel;
+        rb.velocity = moveVector;
+    }
+}
