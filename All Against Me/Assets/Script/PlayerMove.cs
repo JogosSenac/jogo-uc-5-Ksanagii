cc2e5c7175da195462dcc17b3e1c44b6a078a9f0
Ksanagii <Sluisfelipe20@outlook.com>
2024-08-13 21:16:24 -0300

    ADD Visual
    adicionei animação ao andar e coloquei cenario

--- All Against Me/Assets/Script/PlayerMove.cs
+++ All Against Me/Assets/Script/PlayerMove.cs
@@ -5,16 +5,20 @@ using UnityEngine;
 using UnityEngine.UIElements;
 
 [RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
 public class PlayerMove : MonoBehaviour
 {
     [SerializeField] private float vel;
     Rigidbody2D rb;
     Vector3 moveVector;
+    Animator animator;
+    SpriteRenderer sprRen;
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
         moveVector = new Vector3();
-        
+        sprRen = GetComponent<SpriteRenderer>();
     }
 
     void Start()
@@ -25,6 +29,36 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        #region ANIMATION
+        if (Input.GetAxisRaw("Horizontal") != 0)
+        {
+            animator.SetLayerWeight(1, 0);
+            animator.SetLayerWeight(3, 0);
+            animator.SetLayerWeight(2, 1);
+            if (Input.GetAxisRaw("Horizontal") == -1)
+            {
+                sprRen.flipX = true;
+            } else { sprRen.flipX = false; }
+
+        }
+        else if (Input.GetAxisRaw("Vertical") != 0)
+        {
+            animator.SetLayerWeight(2, 0);
+            if (Input.GetAxisRaw("Vertical") == 1)
+            {
+                animator.SetLayerWeight(1, 1);
+            }
+            else { animator.SetLayerWeight(3, 1); }
+
+        }
+        else 
+        { 
+            animator.SetLayerWeight(1, 0);
+            animator.SetLayerWeight(2, 0);
+            animator.SetLayerWeight(3, 0);
+        }
+        #endregion
+
         moveVector.x = Input.GetAxisRaw("Horizontal");
         moveVector.y = Input.GetAxisRaw("Vertical");
         moveVector *= vel;
